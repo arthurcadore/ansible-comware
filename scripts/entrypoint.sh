4986e6ac507b037a7bf90f215b4beacd612b1f2e
arthurcadore <arthurbarcella.ifsc@gmail.com>
2025-08-31 18:39:47 +0000

    :sparkles: added new playbooks to main

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
 echo "Starting Ansible playbooks..."
-ansible-playbook -i /inventory/hosts.yml /playbooks/switch.yml
+# ansible-playbook -i /inventory/hosts.yml /playbooks/modules/aaa.yml
+# ansible-playbook -i /inventory/hosts.yml /playbooks/modules/acl.yml
+ansible-playbook -i /inventory/hosts.yml /playbooks/modules/bfd.yml
