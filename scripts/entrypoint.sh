a62bd58adee575a3e2ffd221da0bbde133586767
arthurcadore <arthurbarcella.ifsc@gmail.com>
2025-08-31 17:32:14 -0300

    :sparkles: added new playbook files to main

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -1,6 +1,20 @@
 #!/bin/bash
 
 echo "Starting Ansible playbooks..."
-# ansible-playbook -i /inventory/hosts.yml /playbooks/modules/aaa.yml
-# ansible-playbook -i /inventory/hosts.yml /playbooks/modules/acl.yml
-ansible-playbook -i /inventory/hosts.yml /playbooks/modules/bfd.yml
+
+# Execute a ping test
+ansible-playbook -v -i /inventory/hosts.yml /playbooks/modules/ping.yml
+
+# Execute a command test
+ansible-playbook -v -i /inventory/hosts.yml /playbooks/modules/command.yml
+
+# Execute a interface state test
+ansible-playbook -v -i /inventory/hosts.yml /playbooks/modules/intfState.yml
+
+# Execute a lldp test
+ansible-playbook -v -i /inventory/hosts.yml /playbooks/modules/lldp_global.yml
+ansible-playbook -v -i /inventory/hosts.yml /playbooks/modules/lldp.yml
+
+# Execute a neighbors test
+ansible-playbook -v -i /inventory/hosts.yml /playbooks/modules/neighbors.yml
+
