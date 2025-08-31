f6f576a468b5c5d7651cf8d1896353b96e51b585
arthurcadore <arthurbarcella.ifsc@gmail.com>
2025-08-31 21:07:46 +0000

    :sparkles: updated main

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -1,4 +1,22 @@
 #!/bin/bash
 
 echo "Starting Ansible playbooks..."
-ansible-playbook -i /inventory/hosts.yml /playbooks/switch.yml
+
+# Execute a ping test
+ansible-playbook -v -i /inventory/hosts.yml /playbooks/modules/ping.yml
+
+# Execute a command test
+ansible-playbook -v -i /inventory/hosts.yml /playbooks/modules/command.yml
+
+# Execute a interface state test
+ansible-playbook -v -i /inventory/hosts.yml /playbooks/modules/intfState.yml
+
+# Execute a lldp test
+ansible-playbook -v -i /inventory/hosts.yml /playbooks/modules/lldp_global.yml
+ansible-playbook -v -i /inventory/hosts.yml /playbooks/modules/lldp.yml
+
+# Execute a neighbors test
+ansible-playbook -v -i /inventory/hosts.yml /playbooks/modules/neighbors.yml
+
+# Execute a snmp community test
+ansible-playbook -v -i /inventory/hosts.yml /playbooks/modules/snmp_community.yml
